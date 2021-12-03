a90b9bac4b3eb144f12fca3dd56ec52b9a1832d3
vin <vin@cybridge.jp>
2021-12-03 21:05:26 +0700

    add-feature

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <comp-header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CompHeader from './components/CompHeader.vue'
 
 export default {
+  components: { CompHeader },
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
+  data() {
+    return {
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/CompHeader.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <h3 @click="handleClick">{{ number }}</h3>
+    <button @click="number += 1">Change title</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comp-header',
+  data() {
+    return {
+      number: 1
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$destroy()
+    }
+  },
+  beforeCreate() {
+    console.log('before create');
+  },
+  create() {
+    console.log('create!');
+  },
+  beforeMount() {
+    console.log('before mount!');
+  },
+  mounted() {
+    console.log('mounted!');
+  },
+  beforeUpdate() {
+    console.log('before update!');
+  },
+  updated() {
+    console.log('updated!');
+  },
+  beforeDestroy() {
+    console.log('before destroy');
+  },
+  destroyed() {
+    console.log('destroyed');
+  }
+
+}
+</script>
+
+<style>
+
+</style>
